d373be30cc4a5bdcd150c007032e5501102b759b
Daniel Merys <dkmerys@gmail.com>
2020-03-31 16:19:09 -0700

    update html and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
-$(document).ready(function() {
-
+$(document).ready(function(){
+  // $("#goodbehavior").hide();
+  // $("#okaybehavior").hide();
+  // $("#badbehavior").hide();
+  $("form#pastbehavior").submit(function(event){
+    event.preventDefault();
+    $("#goodbehavior").hide();
+    $("#okaybehavior").hide();
+    $("#badbehavior").hide();
+    $("input:checkbox[name=behavior]:checked").each(function(){
+        if ($("input[type:checkbox]:checked").length <= 2) {
+          $("#goodbehavior").show()
+          } else if ($("input[type:checkbox]:checked").length > 2 && $("input[type:checkbox]:checked").length <= 6) {
+            $("#okaybehavior").show()
+            } else if ($("input[type:checkbox]:checked").length > 6) {
+              $("#badbehavior").show()
+            }
+            $("#pastbehavior").hide();
+    });
+  });
 });
